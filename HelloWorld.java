962c17241469a4b6f1615a8140fa8080c3b3494e
conor-williams <116696722+conor-williams@users.noreply.github.com>
2022-12-07 15:25:01 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld {
       public static void main(String [] args) {
           System.out.println("Hello World");
       }
-      public static Integer tobetested(String foo) {
+      public static int tobetested(String foo) {
           if (foo.equals("Hello")) return 1;
           else return 0;
       }
