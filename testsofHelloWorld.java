d2432ef55b5853b1dffa9c9c8b5aeb03c3c52902
conor-williams <116696722+conor-williams@users.noreply.github.com>
2022-12-07 16:33:16 +0000

    Update testsofHelloWorld.java

--- testsofHelloWorld.java
+++ testsofHelloWorld.java
@@ -1,9 +1,9 @@
-import static junit.framework.Assert.assertEquals;  
-import junit.framework.Test;  
+import static org.junit.framework.Assert.assertEquals;  
+import org.junit.framework.Test;  
 public class testsofHelloWorld {  
            
     HelloWorld obj = new HelloWorld();  
-     
+    @Test
     public void testHelloWorld1() {  
         assertEquals("Test of return in junit", 1, obj.tobetested("Hello"));  
     }  
